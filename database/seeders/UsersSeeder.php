c71f8243560a074c32e23931029f06bbfccdc724
Yan Menezes <pignata.yan@gmail.com>
2024-12-20 22:55:19 -0300

    ajustes

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -13,9 +13,9 @@ class UsersSeeder extends Seeder {
     public function run()
     {
         $usuario = User::create(Array(
-            'username' => 'kame',
-            'name' => "kame",
-            'email' => "lucassalvino1@gmail.com",
+            'username' => 'Menezes',
+            'name' => "Menezes",
+            'email' => "pignata.yan@gmail.com",
             'path_avatar' => User::$pathAvatarPadrao,
             'password' => 'nds'
         ));
